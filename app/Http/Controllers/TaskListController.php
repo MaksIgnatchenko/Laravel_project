31b718620511c9f1241b76b3d8f6afab5ededeb4
Trojanez <trojanez758@gmail.com>
2018-07-05 21:08:09 +0300

    Merge branch 'master' of gitlab.a-level.com.ua:gitgod/sourceplanet
    # Conflicts:
    #	app/Http/Controllers/GoogleController.php
    #	resources/views/auth/login.blade.php
    #	routes/web.php

--- app/Http/Controllers/TaskListController.php
+++ app/Http/Controllers/TaskListController.php
@@ -7,17 +7,20 @@ use http\Env\Response;
 use Illuminate\Http\Request;
 use App\Tasklist;
 use App\Task;
+use Input;
 use Illuminate\Support\Facades\DB;
 
 class TasklistController extends Controller
 {
     public function index()
     {
-        /*$tasklists = DB::table('tasklist_task')->join('tasklists', 'tasklists.id', '=', 'tasklist_task.task_id')->orderBy('order_id', 'DESC')->get();*/
+        $tasklists = DB::table('tasklists')
+            ->join('tasklist_task', 'tasklists.id', '=', 'tasklist_task.tasklist_id')
+            ->join('tasks', 'tasks.id', '=', 'tasklist_task.task_id')
+            ->orderBy('order_id')
+            ->get();
 
-        $tasklists = Tasklist::all();
-        /*dd($tasklists);*/
-
+        dd($tasklists);
         return view('adminTasklist', compact('tasklists'));
     }
 
@@ -41,17 +44,10 @@ class TasklistController extends Controller
 
     public function menuStore(Request $request)
     {
+        $tasks = explode(',', $_POST['order']);
 
-        if($request->query) {
-            dd($request->all());
-            $id_ary = explode(",",$request->row_order);
-            for($i=0;$i<count($id_ary);$i++) {
-                TasklistTask::where(['task_id' => $id_ary[$i], 'tasklist_id' => $request->tasklist_id])->update(['order_id' => $i]);
-            }
+        foreach($tasks as $key => $value){
+            TasklistTask::where(['task_id' => $value, 'tasklist_id' => $_POST['id']])->update(['order_id' => $key+1]);
         }
-
-          return redirect()->route('tasklists');
-
-
     }
 }
