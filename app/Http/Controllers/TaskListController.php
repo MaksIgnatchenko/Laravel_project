277af85e2dda87fa3c900900739ad00147a38269
Taras <tar_rudenko@ukr.net>
2018-07-05 22:51:58 +0300

    Merge branch 'max' into taras
    # Conflicts:
    #	app/Http/Controllers/TaskListController.php
    #	resources/views/adminTasklist.blade.php

---
+++ app/Http/Controllers/TaskListController.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Http\Controllers;
+use App\TasklistTask;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use App\Tasklist;
+use App\Task;
+use Illuminate\Support\Facades\DB;
+class TasklistController extends Controller
+{
+    public function index()
+    {
+        $tasklists = DB::table('tasklists')
+            ->join('tasklist_task', 'tasklists.id', '=', 'tasklist_task.tasklist_id')
+            ->join('tasks', 'tasks.id', '=', 'tasklist_task.task_id')
+            ->orderBy('order_id')
+            ->get();
+        dd($tasklists);
+        return view('adminTasklist', compact('tasklists'));
+    }
+    public function create(Request $request)
+    {
+        $tasklist = new Tasklist();
+        $tasklist->name = $request->tasklistName;
+        $tasklist->save();
+        return redirect()->route('tasklists');
+    }
+    public function addTask(Request $request)
+    {
+        $tasklist = TaskList::find($request->tasklist_id);
+        $task = Task::where('id', $request->task_id)->first();
+        $tasklist->tasks()->attach($task->id);
+        return redirect()->route('tasklists');
+    }
+    public function menuStore(Request $request)
+    {
+        $tasks = explode(',', $_POST['order']);
+        foreach($tasks as $key => $value){
+            TasklistTask::where(['task_id' => $value, 'tasklist_id' => $_POST['id']])->update(['order_id' => $key+1]);
+        }
+    }
+}
---
+++ resources/views/adminTasklist.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.layout_admin')
+@section('sidebar')
+    <nav class='animated bounceInDown nav'>
+        @foreach($tasklists as $tasklist)
+            <ul>
+                <li class='sub-menu'>
+                    <a id="to-sort" href='#settings'>
+                        <div class='fa fa-user'></div>
+                        {{$tasklist->name}}
+                        <div class='fa fa-caret-down right'></div>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href='#'>
+                                <form action="{{ action('TasklistController@addTask') }}" method="POST">
+                                    {{csrf_field()}}
+                                    <div class="group">
+                                        <input type="text" required name="task_id">
+                                        <span class="highlight"></span>
+                                        <span class="bar"></span>
+                                        <label>Task id</label>
+                                        <input id="tasklist_id" type="hidden" name="tasklist_id" value="{{ $tasklist->id }}">
+                                        <br>
+                                        <input class="litel" type="submit" value="Добавить задачу">
+                                    </div>
+                                </form>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul id="sortable-row">
+                        @foreach($tasklist->tasks as $task)
+                            <li id="{{$tasklist->id}}:{{$task->id}}">
+                                <a href='#'>
+                                    {{$task->task_desc}}
+                                </a>
+                            </li>
+                        @endforeach
+                        <input id="{{ $tasklist->id }}" type="submit" class="btnSave" name="submit" value="Save Order" onClick="saveOrder(this.id);" />
+                    </ul>
+                </li>
+            </ul>
+        @endforeach
+        <ul>
+            <li class='sub-menu'>
+                <a href='#settings'>
+                    <div class='fa fa-user'></div>
+                    Создать новый задачник
+                    <div class='fa fa-caret-down right'></div>
+                </a>
+                <ul>
+                    <li>
+                        <form action="{{ action('TasklistController@create') }}" method="POST">
+                            {{csrf_field()}}
+                            <input type="text" required name="tasklistName">
+                            <span class="highlight"></span>
+                            <span class="bar"></span>
+                            <label>New tasklist</label>
+                            <br>
+                            <input class="litel" type="submit" value="Создать задачник">
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <script>
+        $("ul#sortable-row").sortable();
+        function saveOrder(id) {
+            var task_list_order = [];
+            $('.sub-menu').find('ul#sortable-row li').each(function() {
+                if($(this).attr("id").slice(':')[0] == id){
+                    task_list_order.push($(this).attr("id"));
+                }
+            });
+            var order = [];
+            for(var prop in task_list_order){
+                order.push(task_list_order[prop].slice(':')[2]);
+            }
+            order = order.join();
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: '{{ route('taskListsStore') }}',
+                cache: false,
+                type: 'POST',
+                data: {
+                    'id' : id,
+                    'order' : order
+                },
+                success: function(){
+                    alert('ok');
+                }
+            })
+        }
+    </script>
+@endsection
