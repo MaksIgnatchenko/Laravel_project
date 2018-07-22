e2f8ac3624219b9f672ccef481ea58fc5727d634
Taras <tar_rudenko@ukr.net>
2018-07-23 01:53:02 +0300

    correct css

--- resources/views/total_marks.blade.php
+++ resources/views/total_marks.blade.php
@@ -3,7 +3,7 @@
 @section('sidebar')
     <table class="simple-little-table">
         <tr>
-            <th style="font-size: 30px">
+            <th style="font-size: 30px;text-shadow: 2px 2px 0px #b5b5b5;">
                 {{$group->name}}
             </th>
         </tr>
@@ -15,13 +15,19 @@
         </tr>
         @foreach($array as $user => $moduls)
             <tr>
-                <td>{{$user}}</td>
+                <td style="font-weight: bold">{{$user}}</td>
                 @foreach($moduls as $user => $rate)
-                <td>
+                    @if(count($rate))
+                <td style="color: #187a54">
                     @foreach($rate as $task)
-                        solved task:{{$task->id}}<br>
+                        solved task: {{$task->id}}<br><br>
                     @endforeach
                 </td>
+                        @else
+                        <td style="color: #bd5a35">
+                            no solutions yet
+                        </td>
+                    @endif
                 @endforeach
             </tr>
         @endforeach
